d5821fde084001d2595eb44d488276e888a8e394
Jim Swain <js198623@ohio.edu>
2025-10-28 17:06:09 -0400

    added more user focused endpoints

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -26,10 +26,25 @@ def read_body(name: str):
 
 @app.get("/user/locations")
 def read_user_locations():
-    locs = get_saved_locations()
-    return locs
+    return get_user_saved_locations()
 
-def get_saved_locations():
+@app.get("/user/preferences")
+def read_user_preferences():
+    return get_user_preferences()
+
+@app.get("/user/saved-events")
+def read_user_saved_events():
+    return get_user_saved_events()
+
+@app.post("/user/locations/add")
+def post_user_saved_location(lon: float, lat: float, elevation: float, name: str):
+    save_user_location(GeodeticLocation(lon, lat, elevation), name)
+
+@app.post("/user/preferences/update")
+def post_update_user_preferences(options: List):
+    update_user_preferences(options)
+
+def get_user_saved_locations():
     return {
         GeodeticLocation(-82, 39, 100),
         GeodeticLocation(-82, 39.2, 200),
