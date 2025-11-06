9eea16eccfd4793573eb7816b0a476a3ce8f11a1
ms608122 <156830998+ms608122@users.noreply.github.com>
2025-11-06 16:13:56 -0500

    added visibility table

--- init_db.py
+++ init_db.py
@@ -1,31 +1,48 @@
 import psycopg2
 from config import config
 
-def create_users_table():
-    create_table_sql = """
-    CREATE TABLE IF NOT EXISTS users (
-        user_id SERIAL PRIMARY KEY,
-        username VARCHAR(50) UNIQUE NOT NULL,
-        email VARCHAR(255) UNIQUE NOT NULL,
-        password_hash VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-    """
+def create_tables():
+    commands = (
+        #users
+        """
+        CREATE TABLE IF NOT EXISTS users ( 
+            user_id SERIAL PRIMARY KEY,
+            username VARCHAR(50) UNIQUE NOT NULL,
+            email VARCHAR(255) UNIQUE NOT NULL,
+            password_hash VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS visibility_requests (
+            request_id SERIAL PRIMARY KEY,
+            user_id INT REFERENCES users(user_id) ON DELETE SET NULL,
+            object_name VARCHAR(100) NOT NULL,
+            input_temperature NUMERIC(6,2),
+            input_date DATE NOT NULL,
+            latitude NUMERIC(9,6),
+            longitude NUMERIC(9,6),
+            visible_at TIMESTAMP,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        """ #visibility_requests
+    )
+
     params = config()
     conn = None
     try:
         conn = psycopg2.connect(**params)
         cur = conn.cursor()
-        cur.execute(create_table_sql)
+        for command in commands:
+            cur.execute(command)
         conn.commit()
         cur.close()
-        print("users table created.")
+        print("tables created.")
     except (Exception, psycopg2.DatabaseError) as error:
-        print("Error creating users table:", error)
+        print("Error creating tables:", error)
     finally:
         if conn is not None:
             conn.close()
 
 if __name__ == "__main__":
-    create_users_table()
-
+    create_tables()
